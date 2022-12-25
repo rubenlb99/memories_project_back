781bf43af4703793d0244feb3db2c47a545dab1f
rubenlb99 <rubenlopez6690@gmail.com>
2022-12-25 16:38:12 +0100

    crud functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js"
 
 export const getPosts = async (req, res) => {
@@ -6,7 +7,7 @@ export const getPosts = async (req, res) => {
 
         res.status(200).json(postMessages)
     } catch (error) {
-        res.status(404).json({ message: error.message })
+        res.status(404).json({ message: error })
     }
    
 
@@ -22,6 +23,39 @@ export const createPost = async (req, res) => {
 
         res.status(201).json(newPost);
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        res.status(409).json({ message: error});
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id")
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true})
+
+    res.json(updatedPost)
+}
+
+export const deletePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id")
+
+    await PostMessage.findByIdAndRemove(_id)
+
+    res.json({ message: 'Post deleted succesfuly' })
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that id")
+
+    const post = await PostMessage.findById(id)
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, { new: true})
+
+    res.json(updatedPost)
 }
